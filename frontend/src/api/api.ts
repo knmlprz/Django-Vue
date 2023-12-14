aa343ebe10e102173de82d8e4c5c3bdded5cbb9b
kosiarn <kosiarn@users.noreply.github.com>
2023-12-14 16:36:14 +0100

    add Axios config to api

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,5 +1,9 @@
 import axios from "axios"
 
 export default(url = 'http://localhost:8000/api') => {
-    return axios.create({baseURL: url})
+    return axios.create({
+        baseURL: url,
+        xsrfCookieName: 'csrftoken',
+        xsrfHeaderName: 'X-CSRFToken',
+        withCredentials: true})
 }
