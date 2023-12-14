ea255e73844b8efc20de29d453c3f0106d73939a
kosiarn <kosiarn@users.noreply.github.com>
2023-12-14 16:50:41 +0100

    add login page

---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-container class="fill-height">
+        <v-responsive class="align-center text-center fill-height">
+            <v-card>
+                <div class="py-14" />
+                <h1 class="text-h2 font-weight-bold">Zaloguj</h1>
+                <div class="py-14" />
+                <v-col>
+                    <v-form>
+                        <v-text-field
+                            clearable
+                            label="E-mail"
+                            v-model="email"
+                            ></v-text-field>
+                        <v-text-field
+                            clearable
+                            label="Hasło"
+                            type="password"
+                            v-model="password"></v-text-field>
+                        <v-btn 
+                            
+                            variant="tonal"
+                            @click="handleLoginButtonClick()">Zaloguj</v-btn>
+                    </v-form>
+                </v-col>
+            </v-card>
+        </v-responsive>
+    </v-container>
+
+</template>
+
+<script setup lang="ts">
+import axios from 'axios';
+import { ref } from 'vue';
+import { login } from '@/api/auth'
+const email = ref<string>('');
+const password = ref<string>('');
+const handleLoginButtonClick = () => {
+    login(email.value, password.value)
+}
+</script>
