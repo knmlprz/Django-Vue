1d11febd711c658ab3e483918346d6d068316ac9
kosiarn <kosiarn@users.noreply.github.com>
2023-12-15 11:41:31 +0100

    add the rest of auth endpoints

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -3,3 +3,15 @@ import api from "./api"
 export const login = (email: string, password: string) => {
     api().post('/auth/login', {"email": email, "password": password})
 }
+
+export const logout = () => {
+    api().post('/auth/logout')
+}
+
+export const getUser = () => {
+    api().get('/auth/user')
+}
+
+export const register = (email: string, password: string, password_confirm: string) => {
+    api().post('/auth/register', {'email': email, 'password': password, 'password_confirm': password_confirm})
+}
